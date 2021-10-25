50e021c43e8a2fb3f21e434dfb1ca60aece3d714
ReVanTis <revantis0@gmail.com>
2021-10-25 18:36:53 +0800

    Fixing start up song not being played in audio_startup() by defining AUDIO_INIT_DELAY.
    Enable command/console.

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
+    #define AUDIO_INIT_DELAY
     #define STARTUP_SONG SONG(PREONIC_SOUND)
     // #define STARTUP_SONG SONG(NO_SOUND)
 
--- keymap.c
+++ keymap.c
@@ -147,7 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_COMM, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_DOT , KC_UP,   KC_ENT,  \
-  KC_LCTL, TT(_FN), KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT  \
+  KC_LCTL, TT(_FN), KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_RSFT, RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT  \
 ),
 /* Lower
  * ,-----------------------------------------------------------------------------------.
